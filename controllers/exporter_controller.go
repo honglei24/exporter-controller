f12e89ab54e52eef2a316f9baf525a4ba5fdb299
honglei <honglei@corp-ci.com>
2020-01-10 17:44:54 +0800

    inject sidecar for app

--- controllers/exporter_controller.go
+++ controllers/exporter_controller.go
@@ -18,6 +18,7 @@ package controllers
 
 import (
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -42,7 +43,9 @@ type ExporterReconciler struct {
 
 // +kubebuilder:rbac:groups=app.ci.com,resources=exporters,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=app.ci.com,resources=exporters/status,verbs=get;update;patch
-
+// +kubebuilder:rbac:groups=apps;extensions,resources=deployments,verbs=get;list;watch;create;update;delete
+// +kubebuilder:rbac:groups="",resources=services;events;configmaps;persistentvolumeclaims,verbs=get;list;watch;create;update;delete;patch
+// +kubebuilder:rbac:groups=extensions,resources=ingresses,verbs=get;list;watch;create;update;delete
 func (r *ExporterReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
 	log := r.Log.WithValues("exporter", req.NamespacedName)
@@ -59,8 +62,6 @@ func (r *ExporterReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		// on deleted requests.
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
-
-	deploy := getDeployFromExporter(instance)
 
 	if instance.DeletionTimestamp != nil {
 		log.Info("Get deleted App, clean up subResources.")
@@ -89,6 +90,8 @@ func (r *ExporterReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 			return ctrl.Result{}, err
 		}
 	}
+
+	deploy := getDeployFromExporter(instance)
 
 	found := &appsv1.Deployment{}
 	err = r.Get(ctx, types.NamespacedName{Name: deploy.Name, Namespace: deploy.Namespace}, found)
@@ -127,7 +130,7 @@ func (r *ExporterReconciler) deleteExternalResources(instance *appv1.Exporter) e
 	// 需要确保实现是幂等的
 	ctx := context.Background()
 	deploy := getDeployFromExporter(instance)
-	r.Log.Info("delete exporter", "exporter", instance)
+	r.Log.Info("Delete exporter", "exporter", instance)
 	err := r.Delete(ctx, deploy)
 	return err
 }
@@ -139,6 +142,36 @@ func getDeployFromExporter(instance *appv1.Exporter) *appsv1.Deployment {
 	deploySpec := appsv1.DeploymentSpec{}
 	deploySpec = instance.Spec.Deploy
 	deploySpec.Selector = &metav1.LabelSelector{MatchLabels: labels}
+
+	sidecar := corev1.Container{
+		Name:            "nginx",
+		Image:           "nginx:latest",
+		ImagePullPolicy: corev1.PullIfNotPresent,
+		VolumeMounts: []corev1.VolumeMount{
+			{
+				Name:      "nginx-default-conf",
+				MountPath: "/etc/nginx/conf.d",
+			},
+		},
+	}
+	volume := corev1.Volume{
+		Name: "nginx-default-conf",
+		VolumeSource: corev1.VolumeSource{
+			ConfigMap: &corev1.ConfigMapVolumeSource{
+				LocalObjectReference: corev1.LocalObjectReference{Name: "nginx-conf"},
+			},
+		},
+	}
+	if deploySpec.Template.ObjectMeta.Annotations == nil {
+		deploySpec.Template.ObjectMeta.Annotations = make(map[string]string)
+	}
+	// 设置注解
+	setDefaultValue(deploySpec.Template.ObjectMeta.Annotations, "prometheus.io/scrape", "true")
+	setDefaultValue(deploySpec.Template.ObjectMeta.Annotations, "prometheus.io/path", "/metrics")
+	setDefaultValue(deploySpec.Template.ObjectMeta.Annotations, "prometheus.io/port", "8081")
+
+	deploySpec.Template.Spec.Containers = append(deploySpec.Template.Spec.Containers, sidecar)
+	deploySpec.Template.Spec.Volumes = append(deploySpec.Template.Spec.Volumes, volume)
 
 	deploy := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
@@ -168,4 +201,10 @@ func removeString(slice []string, s string) (result []string) {
 		result = append(result, item)
 	}
 	return
+}
+
+func setDefaultValue(m map[string]string, key string, value string) {
+	if _, ok := m[key]; !ok {
+		m[key] = value
+	}
 }
